722a1214178d3975943cd40288cd99f112a52646
HarshitDolu <jainmanish9434@gmail.com>
2020-05-01 06:11:30 +0530

    added decorators, django usercreationforms

--- employer recommendation/dbase/emp/views.py
+++ employer recommendation/dbase/emp/views.py
@@ -1,107 +1,174 @@
-from django.shortcuts import render,HttpResponse,redirect
-from .models import student,employer,jobs
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, HttpResponse, redirect
+from .models import student, employer, jobs, appliedjobs
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+from .decorators import unauthenticated_user, allowed_users, employers_only
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
+from django.contrib.auth.models import Group
+
 
 def index(request):
-    return render(request,'emp/main_index.html')
-def apply(request):
+    if request.user.is_authenticated:
+        return redirect('student')
+    return render(request, 'emp/main_index.html')
 
-    if request.method =='POST':
+
+# student section
+def apply(request):
+    if request.method == 'POST':
         name = request.POST['name']
         skills = request.POST['skills']
-        share= request.POST['share']
-        print(name,skills,share)
+        share = request.POST['share']
+        print(name, skills, share)
 
-        if len(name)<2 or len(skills)==0:
-            messages.error(request,"Please fill it correctly")
+        if len(name) < 2 or len(skills) == 0:
+            messages.error(request, "Please fill it correctly")
         else:
             stud = student(name=name, skills=skills, experience=share)
             stud.save()
-            messages.success(request,"Profile has been updated")
-    return render(request,'emp/apply.html')
+            messages.success(request, "Profile has been updated")
+    return render(request, 'emp/apply.html')
+
+
+# company sections
 def post(request):
     messages.success(request, 'This is employers section')
-    if request.method=='POST':
-        name=request.POST['name']
-        title=request.POST['title']
-        location=request.POST['location']
-        skills=request.POST['skills']
-        share=request.POST['share']
-        cmp=employer(company_name=name)
+    if request.method == 'POST':
+        name = request.POST['name']
+        title = request.POST['title']
+        location = request.POST['location']
+        skills = request.POST['skills']
+        share = request.POST['share']
+        cmp = employer(company_name=name)
         cmp.save()
 
-        jb=jobs(jobtitle=title,joblocation=location,jobskills=skills,jobdescription=share,employer=cmp)
+        jb = jobs(jobtitle=title, joblocation=location, jobskills=skills, jobdescription=share, employer=cmp)
         jb.save()
-    return render(request,'emp/post.html')
+    return render(request, 'emp/post.html')
+
 
 def results(request):
-    alljobs=jobs.objects.all()
-    context={'alljobs':alljobs}
-    #print(alljobs)
-    #if request.method=='POST':
+    alljobs = jobs.objects.all()
+    context = {'alljobs': alljobs}
+    # print(alljobs)
+    # if request.method=='POST':
 
-    return render( request,'emp/results.html',context)
+    return render(request, 'emp/results.html', context)
+
+
+# o(n^2) algo for recommending
 def recommend(request):
-    alljobs=jobs.objects.all()
-    stud=student.objects.all()
+    alljobs = jobs.objects.all()
+    stud = student.objects.all()
     for job in alljobs:
         for st in stud:
-            dj=job.jobskills.split(',')
-            ds=st.skills.split(',')
+            dj = job.jobskills.split(',')
+            ds = st.skills.split(',')
             for s in ds:
                 if s in dj:
-                    print(job.jobtitle,st.name)
+                    print(job.jobtitle, st.name)
 
-    return render(request,'emp/recommend.html')
-
-def signup(request):
-
-    if request.method=='POST':
-        username=request.POST['username']
-        email=request.POST['email']
-        password1=request.POST['password1']
-        password2=request.POST['password2']
-        if password1!=password2:
-            return redirect('index')
-        myuser = User.objects.create_user(username, email, password1)
-        myuser.save()
-        return redirect('login')
+    return render(request, 'emp/recommend.html')
 
 
-    else:
-
-        return render(request,'emp/signup.html')
-
-
+# login
 def signin(request):
-    if request.method == 'POST':
-     # get post parameters
-        loginusername=request.POST['loginusername']
-        loginpassword=request.POST['loginpassword']
+    if request.user.is_authenticated:
+        return redirect('student')
+    else:
+        if request.method == 'POST':
+            # get post parameters
+            username = request.POST['username']
+            password = request.POST['password']
 
-        user=authenticate(username=loginusername,password=loginpassword)
+            user = authenticate(username=username, password=password)
 
-        if user is not None:
-            login(request,user)
-        #message
-            return redirect('student')
-        else:
-       #message
-            return redirect('index')
-    return render(request,'emp/login.html')
+            if user is not None:
+                login(request, user)
+                # message
+                return redirect('student')
+            else:
+                # message
+                messages.info(request, 'username or password is incorrect')
+        return render(request, 'emp/login.html')
 
 
+@login_required(login_url='login')
+@employers_only
+@allowed_users(allowed_roles=['students'])
+# only students
 def student(request):
-    return render(request,'emp/student_section.html')
+    p = appliedjobs.objects.all()
+    name = request.user.student.name
+
+    print(p)
+    return render(request, 'emp/student_section.html')
+
+
+# only students
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['students'])
+def student_profile(request):
+    # work in progress
+    name = request.user.student.jobs.all()
+    print(name)
+
+
+# decorators to allow only employer
+@allowed_users(allowed_roles=['employer'])
 def company(request):
-    return render(request,'emp/employer_section.html')
+    return render(request, 'emp/employer_section.html')
 
 
+# logout
 def handlelogout(request):
-
     logout(request)
     return redirect('index')
     return HttpResponse('404 Not found')
+
+
+# sign up page
+
+def registerpage(request):
+    if request.user.is_authenticated:
+        return redirect('student')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                username = form.cleaned_data.get('username')
+                group = Group.objects.get(name='students')
+                user.groups.add(group)
+                messages.success(request, 'Account was created for ' + username)
+                return redirect("login")
+        context = {'form': form}
+        return render(request, 'emp/reg.html', context)
+
+    # temporary login page
+
+
+def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('student')
+    else:
+        if request.method == 'POST':
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'username or password is incorrect')
+
+        return render(request, 'emp/login.html')
+
