c88e5ad181177a0b67cbd56720cb75c455a4fb6b
HarshitDolu <jainmanish9434@gmail.com>
2020-05-06 23:38:36 +0530

    added urls for appliedjobs

--- employer recommendation/dbase/emp/urls.py
+++ employer recommendation/dbase/emp/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from  . import views
+from .views import GeneratePdf
 urlpatterns = [
+    path('apply/jobsapplied/<str:pk>/',views.jobs_applied,name="appliedjobs"),
+
+    path('search',views.searchjob,name='search'),
+    path('pdf',views.GeneratePdf,name="gp"),
     path('',views.index,name="index"),
-    path('apply/', views.apply, name="apply"),
-    path('post_jobs',views.create_job,name="post_jobs"),
+    path('apply/', views.apply_jobs, name="apply"),
+    path('post_jobs',views.postjob,name="post_jobs"),
     path('results/',views.recommend,name="results"),
     path('profile_page',views.student_page,name='profile_page'),
     path('signup_student/',views.registerpage_student,name='signup1'),
@@ -14,10 +19,10 @@ urlpatterns = [
     path('deletejob/<str:pk>/', views.delete_job, name='update_job'),
     path('login',views.signin,name='login'),
     path('logout', views.handlelogout, name='logout'),
-    path('roles',views.roles,name='roles'),
+
     path('students',views.studentpg,name="student"),
     path('employer', views.company, name="employer"),
-    path('student_profile',views.profile,name='profile'),
+    path('student_profile',views.student_profile,name='student_profile'),
     path('employer_profile', views.employer_profile, name='emprofile'),
     path('reset_password/',auth_views.PasswordResetView.as_view(template_name="emp/password_reset.html"),name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="emp/password_reset_sent.html"),name="password_reset_done"),
